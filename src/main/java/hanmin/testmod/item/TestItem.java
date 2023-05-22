c1615b1b3ccb4c7a439a9d36c24a3d26bbf97b88
dlharry <dlharry@126.com>
2023-05-22 22:54:17 +0100

    Restructure TestItems

--- src/main/java/hanmin/testmod/item/TestItem.java
+++ src/main/java/hanmin/testmod/item/TestItem.java
@@ -4,6 +4,7 @@ import java.util.function.Supplier;
 
 import hanmin.testmod.TestMod;
 import hanmin.testmod.item.custom.CustomArrow;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -22,79 +23,75 @@ public class TestItem extends Item {
 	);
 	
 	public static RegistryObject<Item> PIG_SPAWNER = ITEMS.register("pig_spawner",() -> {
-		return new PigSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.PIG);}
 	);
-	
-
-
 
 	public static RegistryObject<Item> CHICKEN_SPAWNER = ITEMS.register("chicken_spawner",() -> {
-		return new ChickenSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.CHICKEN);}
 	);
 
 	public static RegistryObject<Item> HORSE_SPAWNER = ITEMS.register("horse_spawner",() -> {
-		return new HorseSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.HORSE);}
 
 	);
 	
 	public static RegistryObject<Item> WOLF_SPAWNER = ITEMS.register("wolf_spawner",() -> {
-		return new WolfSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.WOLF);}
 
 	);
 
-
 	public static RegistryObject<Item> CAT_SPAWNER = ITEMS.register("cat_spawner",() -> {
-		return new CatSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.CAT);}
 
 	);
 	
 	public static RegistryObject<Item> VILLAGER_SPAWNER = ITEMS.register("villager_spawner",() -> {
-		return new VillagerSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.VILLAGER);}
 
 	);
 	
 	public static RegistryObject<Item> RABBIT_SPAWNER = ITEMS.register("rabbit_spawner",() -> {
-		return new RabbitSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.RABBIT);}
 
 	);
 	
 	public static RegistryObject<Item> TURTLE_SPAWNER = ITEMS.register("turtle_spawner",() -> {
-		return new TurtleSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.TURTLE);}
 
 	);
 
 	public static RegistryObject<Item> FOX_SPAWNER = ITEMS.register("fox_spawner",() -> {
-		return new FoxSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.FOX);}
 
 	);
 	
 	public static RegistryObject<Item> BAT_SPAWNER = ITEMS.register("bat_spawner",() -> {
-		return new BatSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.BAT);}
 
 	);
 
 	public static RegistryObject<Item> ZOMBIE_SPAWNER = ITEMS.register("zombie_spawner",() -> {
-		return new ZombieSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.ZOMBIE);}
 
 	);
 
 	public static RegistryObject<Item> DOLPHIN_SPAWNER = ITEMS.register("dolphin_spawner",() -> {
-		return new DolphinSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.DOLPHIN);}
 
 	);
 
 	public static RegistryObject<Item> DONKEY_SPAWNER = ITEMS.register("donkey_spawner",() -> {
-		return new DonkeySpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.DONKEY);}
 
 	);
 
 	public static RegistryObject<Item> FROG_SPAWNER = ITEMS.register("frog_spawner",() -> {
-		return new FrogSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.FROG);}
 
 	);
 
 	public static RegistryObject<Item> LLAMA_SPAWNER = ITEMS.register("llama_spawner",() -> {
-		return new LlamaSpawner(new Properties());}
+		return new TestSpawner(new Properties(), EntityType.LLAMA);}
 
 	);
 	public static void register(IEventBus eventBus) {
--- src/main/java/hanmin/testmod/item/TestSpawner.java
+++ src/main/java/hanmin/testmod/item/TestSpawner.java
@@ -1,12 +1,6 @@
 package hanmin.testmod.item;
 
 import hanmin.testmod.block.TestBlock;
-
-
-
-import com.ibm.icu.impl.number.Properties;
-
-import hanmin.testmod.block.TestBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
@@ -15,32 +9,27 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.animal.Fox;
-import net.minecraft.world.entity.animal.Pig;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.level.ClipContext;
-import net.minecraft.world.level.ClipContext.Fluid;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
-
-
-public class FoxSpawner extends Item{
-
-    public FoxSpawner(Properties properties) {
+public class TestSpawner extends Item {
+	
+	private EntityType<?> entitytype;
+	
+    public TestSpawner(Properties properties, EntityType<?> entitytype) {
         super(properties);
+        this.entitytype = entitytype;
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        System.out.println("fox spawner use method called");
+        System.out.println("dolphin spawner use method called");
         ItemStack itemstack = player.getItemInHand(hand);
 
         HitResult hitresult = getPlayerPOVHitResult(level, player, ClipContext.Fluid.SOURCE_ONLY);
@@ -56,8 +45,7 @@ public class FoxSpawner extends Item{
             if (level.getBlockState(blockpos).getBlock() instanceof TestBlock) {
                 System.out.println("second if");
 
-                EntityType<Fox> entitytype = EntityType.FOX;
-                Entity entity = entitytype.spawn((ServerLevel)level, itemstack, player, blockpos.above(), MobSpawnType.SPAWN_EGG, false, false);
+                Entity entity = this.entitytype.spawn((ServerLevel)level, itemstack, player, blockpos.above(), MobSpawnType.SPAWN_EGG, false, false);
                 if (entity == null) {
                     System.out.println("null entity");
                     return InteractionResultHolder.pass(itemstack);
@@ -78,10 +66,4 @@ public class FoxSpawner extends Item{
             }
         }
     }
-
-    //private HitResult getPlayerPOVHitResult(Level level, Player player, Fluid sourceOnly) {
-    // TODO Auto-generated method stub
-    //return null;
-    //}
-
 }
